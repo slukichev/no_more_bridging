e3d0340f7829a98a8b9438e2b05ace319a1e0658
slukichev <semyon.lukichev@gmail.com>
2023-01-05 22:34:07 -0500

    set up mod work space

---
+++ src/main/java/net/sem/no_more_bridging/NoMoreBridgingClient.java
@@ -0,0 +1,10 @@
+package net.sem.no_more_bridging;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NoMoreBridgingClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
